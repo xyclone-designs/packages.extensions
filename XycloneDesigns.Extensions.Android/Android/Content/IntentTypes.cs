edb19a5caa4317f2144b4b1c95d989c7b131cae9
Thando Moeketsi Ndlovu <thandomndlovu@gmail.com>
2025-04-05 03:29:14 +0200

    IntentType Extension Added

--- XycloneDesigns.Extensions.Android/Android/Content/IntentTypes.cs
+++ XycloneDesigns.Extensions.Android/Android/Content/IntentTypes.cs
@@ -25,4 +25,37 @@ namespace Android.Content
 		Video_MP4,
 		Video_3GP,
 	}
+
+	public static class IntentTypesExtensions
+	{
+		public static string? AsIntentType(this IntentTypes intenttype)
+		{
+			return intenttype switch
+			{
+				IntentTypes.All => "*/*",
+				IntentTypes.Application => "application/*",
+				IntentTypes.Application_PDF => "application/pdf",
+				IntentTypes.Audio => "audio/*",
+				IntentTypes.Audio_FLAC => "audio/flac",
+				IntentTypes.Audio_M4A => "audio/m4a",
+				IntentTypes.Audio_MP3 => "audio/mp3",
+				IntentTypes.Audio_WAV => "audio/wav",
+				IntentTypes.Image => "image/*",
+				IntentTypes.Image_GIF => "image/gif",
+				IntentTypes.Image_JPG => "image/jpg",
+				IntentTypes.Image_JPEG => "image/jpeg",
+				IntentTypes.Image_PNG => "image/png",
+				IntentTypes.Text => "text/*",
+				IntentTypes.Text_HTML => "text/html",
+				IntentTypes.Text_JSON => "text/json",
+				IntentTypes.Text_Plain => "text/plain",
+				IntentTypes.Text_RTF => "text_rtf/text_rtf",
+				IntentTypes.Video => "video/*",
+				IntentTypes.Video_MP4 => "video/mp4",
+				IntentTypes.Video_3GP => "video/3gp",
+
+				_ => null
+			};
+		}
+	}
 }
